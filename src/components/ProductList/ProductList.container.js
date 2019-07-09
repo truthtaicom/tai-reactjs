00e378c847dc634226d5705b81deb32ca018fc85
tai <tai@tais-MacBook-Pro.local>
2019-07-09 20:26:38 +0700

    feat pwa

--- src/components/ProductList/ProductList.container.js
+++ src/components/ProductList/ProductList.container.js
@@ -2,9 +2,14 @@ import { connect } from 'react-redux'
 import { addItemToCart } from './ProductList.action'
 import ProductList from './'
 
+export const prmotedProductsSelector = (state) => {
+  return state.productListReducer.data.filter(elm => elm.price > elm.final_price)
+}
+
 const mapStateToProps = (state) => {
   return {
-    selectedItems: state.productListReducer.selectedItems
+    selectedItems: state.productListReducer.selectedItems,
+    proomotedProducts: prmotedProductsSelector(state)
   }
 }
 
